3d2bbf7ab700107e07bbaa4ac525249071214688
ddnnss <ddnnss.i1@gmail.com>
2018-12-28 18:16:13 +0300

    2

--- crm/views.py
+++ crm/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from client.models import Client
+from order.models import Order
 from customuser.models import User
 from order.forms import NewOrderForm
 
 @login_required
 def index(request):
-    all_clients = Client.objects.all()
+    if request.POST:
+        form = NewOrderForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # do something.
+        else:
+            new_order_form = NewOrderForm()
+
+        return HttpResponseRedirect('/crm/')
+
+
+    all_orders = Order.objects.all()
     new_order_form = NewOrderForm()
+
     return render(request, 'crm/index.html', locals())
 
--- order/forms.py
+++ order/forms.py
@@ -6,10 +6,25 @@ from django import forms
 class NewOrderForm(ModelForm):
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['order_temp','tirag','total_price']
 
 
         widgets = {
-             'order_temp': forms.Textarea(attrs={'class': 'form-control'}),
+             'date': forms.DateTimeInput (attrs={'class': 'form-control'}),
+            'client': forms.Select(attrs={'class': 'form-control'}),
+            'manager': forms.Select(attrs={'class': 'form-control'}),
+            'performer': forms.Select(attrs={'class': 'form-control'}),
+            'total_price': forms.TextInput(attrs={'class': 'form-control'}),
+            'discount': forms.TextInput(attrs={'class': 'form-control'}),
+            'tirag': forms.TextInput(attrs={'class': 'form-control'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control'}),
+            'maket': forms.FileInput(attrs={'class': 'form-control'}),
+            'dead_time': forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'is_vip': forms.CheckboxInput(attrs={'class': 'form-control'}),
+            'delivery': forms.CheckboxInput(attrs={'class': 'form-control'}),
+            'delivery_address': forms.Textarea(attrs={'class': 'form-control'}),
+            'order_temp': forms.Textarea(attrs={'class': 'form-control'}),
+
+
          }
 
